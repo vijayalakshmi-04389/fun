a5071d545ab01df867e50e6c0ffc3248b735c5bb
vijayalakshmi-04389 <vijayalakshmi04389@gmail.com>
2026-01-08 18:33:53 +0530

    Update birthday time in countdown script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const birthday = new Date("2026-01-09T00:00:00");
+const birthday = new Date("2026-01-09T18:34:00");
 const timerDisplay = document.getElementById("timer");
 const countdownContent = document.getElementById("countdownContent");
 const birthdayMessage = document.getElementById("birthdayMessage");
